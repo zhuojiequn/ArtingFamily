bae3e0ec9a4812590620ef17a574e555d00e3d0c
zhuojiequn <461332496@qq.com>
2018-06-27 22:28:11 +0800

    no message

--- artingfamily-front/src/components/excellent.js
+++ artingfamily-front/src/components/excellent.js
@@ -1,16 +1,10 @@
 import React,{Component} from 'react';
 
-export default class Home extends Component{
+export default class Excellent extends Component{
     render(){
-        var changeBack = {background:'#f4f4f4',minHeight:'300px',marginTop:'-210px',paddingTop:'350px'}
         return(
-            <div className="Home">
-                <Header />
-                <Slider />
-                <AboutUs />
-                <div style={changeBack}>
-                    <Edution />
-                </div>
+            <div className="excellent">
+                
             </div>
         )
     }
