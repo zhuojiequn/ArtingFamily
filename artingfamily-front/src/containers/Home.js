736f96609f6f16fba3f96afee5631ea0f8fb9dfa
DESKTOP-E6T9768\Admin <1761676396@qq.com>
2018-06-28 09:11:49 +0800

    no message

--- artingfamily-front/src/containers/Home.js
+++ artingfamily-front/src/containers/Home.js
@@ -1,10 +1,12 @@
 import React,{Component} from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer'
 export default class Home extends Component{
     render(){
         return(
             <div className="Home">
                 <Header />
+                <Footer/>
             </div>
         )
     }
