52a2164bb7f6b2a07576f3f48296390e38cc7515
zhuojiequn <461332496@qq.com>
2018-06-28 20:23:50 +0800

    no message

--- artingfamily-front/src/components/header.js
+++ artingfamily-front/src/components/header.js
@@ -4,6 +4,11 @@ import '../public/scss/base.scss';
 import './header.scss';
 
 export default class Header extends Component{
+    componentDidMount(){
+        $('.navbar-left li>a').on('click',function(){
+            $(this).addClass('active').parent('li').siblings('li').find('a').removeClass('active');
+        });
+    }
     render(){
         return(
             <nav className="navbar navbar-fixed-top navbar-has-shadow">
@@ -21,19 +26,19 @@ export default class Header extends Component{
                             </a>
                     </div>
                     {/* 菜单 */}
-                    <div className="collapse navbar-collapse" id="navbar-collapse">
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul className="nav navbar-nav navbar-left">
-                            <li><Link className="spacing active" data-track data-track-location='header' to="/">首<span>页</span></Link></li>
-                            <li><Link className="spacing" data-track data-track-location='header' to="/">陶<span>瓷</span></Link></li>
-                            <li><Link className="spacing" data-track data-track-location='header' to="/">剪<span>纸</span></Link></li>
-                            <li><Link className="spacing" data-track data-track-location='header' to="/">书<span>法</span></Link></li>
-                            <li><Link data-track data-track-location='header' to="/">报渠道名</Link></li>
-                            <li><Link data-track data-track-location='header' to="/">线上培训</Link></li>
-                            <li><Link className="spacing" data-track data-track-location='header' to="/">剪<span>纸</span></Link></li>
+                            <li><Link className="spacing active"  to="/">首<span>页</span></Link></li>
+                            <li><Link className="spacing"  to="/">陶<span>瓷</span></Link></li>
+                            <li><Link className="spacing"  to="/">剪<span>纸</span></Link></li>
+                            <li><Link className="spacing"  to="/">书<span>法</span></Link></li>
+                            <li><Link to="/">报渠道名</Link></li>
+                            <li><Link  to="/">线上培训</Link></li>
+                            <li><Link className="spacing"  to="/">剪<span>纸</span></Link></li>
                         </ul>
                         <ul className="nav navbar-nav navbar-right">
-                            <li><Link data-track data-track-location='header' to="/" >登录</Link></li>
-                            <li><Link data-track data-track-location='header' to="/"  track-event='Clicked Get Started'>注册</Link></li>
+                            <li><Link  to="/" >登录</Link></li>
+                            <li><Link  to="/"  track-event='Clicked Get Started'>注册</Link></li>
                         </ul>
                     </div>
                 </div>
