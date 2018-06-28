db27fde55c68404187bd82cdad9b7f592dced2bf
DESKTOP-E6T9768\Admin <1761676396@qq.com>
2018-06-28 09:47:43 +0800

    121

--- artingfamily-front/src/containers/Home.js
+++ artingfamily-front/src/containers/Home.js
@@ -1,13 +1,22 @@
 import React,{Component} from 'react';
-import Header from '../components/Header';
+import Header from '../components/header';
+import Slider from '../components/slider';
+import AboutUs from '../components/about-us';
+import Edution from '../components/edution';
 import Footer from '../components/Footer'
+
 export default class Home extends Component{
     render(){
         var changeBack = {background:'#f4f4f4',minHeight:'300px',marginTop:'-210px',paddingTop:'350px'}
         return(
             <div className="Home">
                 <Header />
-                <Footer/>
+                <Slider />
+                <AboutUs />
+                <div style={changeBack}>
+                    <Edution />
+                </div>
+                <Footer />
             </div>
         )
     }
